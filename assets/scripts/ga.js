e299958a2cc395f036c145551d4dd1116298fdcf
Fritz Lekschas <service@lekschas.de>
2017-11-23 13:56:35 -0500

    Update

--- assets/scripts/ga.js
+++ assets/scripts/ga.js
@@ -1,2 +1,2 @@
-!function(e,a,t,n,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=a.createElement(t),s=a.getElementsByTagName(t)[0],o.async=1,o.src=n,s.parentNode.insertBefore(o,s)}(window,document,"script","//www.google-analytics.com/analytics.js","ga"),ga("create","UA-72219228-2","auto"),ga("send","pageview");
+!function(e,a,t,n,g,c,o){e.GoogleAnalyticsObject=g,e.ga=e.ga||function(){(e.ga.q=e.ga.q||[]).push(arguments)},e.ga.l=1*new Date,c=a.createElement(t),o=a.getElementsByTagName(t)[0],c.async=1,c.src="//www.google-analytics.com/analytics.js",o.parentNode.insertBefore(c,o)}(window,document,"script",0,"ga"),ga("create","UA-72219228-2","auto"),ga("send","pageview");
 //# sourceMappingURL=ga.js.map
